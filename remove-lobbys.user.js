3331fee777dc1fc84391d7f458d5a750d875582d
Brian Ardiles <ardiles.brian@gmail.com>
2021-09-01 00:05:10 -0300

    Update remove-lobbys.user.js

--- remove-lobbys.user.js
+++ remove-lobbys.user.js
@@ -8,7 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?domain=gamersclub.com.br
 // @grant        none
 // ==/UserScript==
-window.addEventListener('load', init, false)
+window.addEventListener('DOMContentLoaded', init, false)
 
 function init() {
     'use strict';
