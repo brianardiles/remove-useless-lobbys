8ae4454eccb150e87a97fbe75158fb2a2f5e7e69
Brian Ardiles <ardiles.brian@gmail.com>
2021-08-31 23:14:05 -0300

    Create remove-lobbys.user.js

---
+++ remove-lobbys.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Remove useless lobbys
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Brian Ardiles
+// @match        https://gamersclub.com.br/lobby
+// @icon         https://www.google.com/s2/favicons?domain=gamersclub.com.br
+// @grant        none
+// ==/UserScript==
+window.addEventListener('load', init, false)
+
+function init() {
+    'use strict';
+    let target = document.querySelector('.list-avaliable-teams');
+    let observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            let lobbys = document.getElementsByClassName('lobby-room-list-item')
+            for (let lobby of lobbys) {
+                let lobbySelected = lobby.querySelector('span:nth-child(2)')
+                if (!checkAllowedCountry(lobbySelected)) {
+                    lobby.style.display = "none"
+                }
+            }
+        });
+    });
+    let config = {
+        childList: true
+    };
+    observer.observe(target, config);
+};
+
+// Delete lobby if the country is not equal to the allowed list :D
+function checkAllowedCountry(lobby) {
+    const allowedList = ["Argentina", "Uruguai", "Chile"]
+    return allowedList.includes(lobby.title)
+}
